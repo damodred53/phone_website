3fd2d401c48233b91667196538d0743145b569da
Florent <florent.guyard@live.fr>
2024-07-25 11:27:21 +0200

    test 2

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const defaultState = {
         "id": 1,
         "name": "iPhone 13",
         "color": "Noir",
-        "image": "src/assets/iphone_13_noir.webp",
+        "image": "/src/assets/iphone_13_noir.webp",
         "price": 999.99,
         "inCart": 1
     },
@@ -25,7 +25,7 @@ const defaultState = {
         "id": 2,
         "name": "Samsung Galaxy S21",
         "color": "Argent",
-        "image": "src/assets/Samsung_Galaxy S21_argent.jpeg",
+        "image": "/src/assets/Samsung_Galaxy S21_argent.jpeg",
         "price": 849.99,
         "inCart": 1
     },
@@ -33,7 +33,7 @@ const defaultState = {
         "id": 3,
         "name": "Google Pixel 6",
         "color": "Blanc",
-        "image": "src/assets/Google_Pixel_6_blanc.avif",
+        "image": "/src/assets/Google_Pixel_6_blanc.avif",
         "price": 799.99,
         "inCart": 1
     },
@@ -41,7 +41,7 @@ const defaultState = {
         "id": 4,
         "name": "OnePlus 9",
         "color": "Bleu",
-        "image": "src/assets/OnePlus_9_bleu.webp",
+        "image": "/src/assets/OnePlus_9_bleu.webp",
         "price": 729.99,
         "inCart": 1
     },
@@ -49,7 +49,7 @@ const defaultState = {
         "id": 5,
         "name": "Sony Xperia 5 III",
         "color": "Vert",
-        "image": "src/assets/Sony_Xperia_5_III_vert.jpg",
+        "image": "/src/assets/Sony_Xperia_5_III_vert.jpg",
         "price": 899.99,
         "inCart": 1
     }
