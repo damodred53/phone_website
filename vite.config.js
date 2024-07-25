730c92ceec1b9c6aa4c7dbac57f0ca9dcf502b49
Florent <florent.guyard@live.fr>
2024-07-25 09:51:24 +0200

    préparation pour le déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress', // Spécifie l'algorithme de compression
+      ext: '.br', // Extension de fichier pour les fichiers compressés
+      verbose: true, // Afficher les informations détaillées sur la compression
+    }),
+  ],
+  base: "/phone_website/", // Base URL de votre application
+});
